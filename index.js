74c9ebef05b19ae97be4d983511eb4497accf4ec
hassannahid105 <infosharemail405@gamil.com>
2025-08-22 17:48:06 +0600

    new

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const { connectToDB } = require("./config/db");
+const productRoutes = require("./routes/productRoutes");
+const app = express();
+const port = 5000 || process.env.PORT;
+// Middle ware
+app.use(express.json());
+app.use(
+  cors({
+    // ? TODO: change origin later
+    origin: ["*", "http://localhost:5173"], // Allow all origins // Allow only this origin
+    // origin: ["*", "https://fantastic-cajeta-384082.netlify.app"],
+    // methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+    // credentials: true, // If using cookies/auth
+  })
+);
+// Connect to DB (only once)
+connectToDB().then(() => console.log("✅ Connected to DB"));
+
+//  ? product routes api
+app.use("/products", productRoutes);
+
+// ? test api
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(port, () => {
+  console.log("listen");
+});
+// Export app for Vercel
+// TODO: open vercel
+module.exports = app;
---
+++ routes/productRoutes.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const router = express.Router();
+const { client } = require("../config/db");
+const { ObjectId } = require("mongodb");
+
+const productCollection = client.db("eazaar").collection("products");
+
+router.get("/", async (req, res) => {
+  try {
+    const result = await productCollection.find().toArray();
+    res.send(result);
+  } catch (error) {
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await productCollection.findOne(query);
+  res.send(result);
+});
+router.post("/", async (req, res) => {
+  try {
+    const data = req.body;
+    const result = await productCollection.insertOne(data);
+    res.send(result);
+  } catch (error) {
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await productCollection.deleteOne(query);
+  res.send(result);
+});
+
+router.patch("/:id", async (req, res) => {
+  const id = req.params.id;
+  const {
+    name,
+    categories,
+    description,
+    price,
+    offerPrice,
+    brand,
+    stock,
+    isFeatured,
+    images,
+    featureDes,
+    discountType,
+    color,
+    tags,
+    dimensions,
+  } = req.body;
+  const query = { _id: new ObjectId(id) };
+  const updateDoc = {
+    $set: {
+      //
+      name: name,
+      categories: categories,
+      description: description,
+      price: price,
+      offerPrice: offerPrice,
+      brand: brand,
+      stock: stock,
+      isFeatured: isFeatured,
+      images: images,
+      featureDes: featureDes,
+      color: color,
+      tags: tags,
+      discountType: discountType,
+      dimensions: dimensions,
+    },
+  };
+  const result = await productCollection.updateOne(query, updateDoc);
+  res.send(result);
+});
+module.exports = router;
